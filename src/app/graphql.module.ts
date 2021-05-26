23fc2bfb80f86e9df359779b5299edb6189d4c90
Alex Denga <hello@albella.co>
2021-05-27 02:12:06 +0300

    Complete

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'http://142.93.114.247:3000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://codetest.albella.co/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
